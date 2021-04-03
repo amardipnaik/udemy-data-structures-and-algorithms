8e86535b18dd98d9b5ed77155119cde1dba80859
amardipnaik <75632409+amardipnaik@users.noreply.github.com>
2021-04-04 01:05:57 +0530

    Update ReverseAString.java

--- 2.Arrays and String/ReverseAString.java
+++ 2.Arrays and String/ReverseAString.java
@@ -3,14 +3,46 @@ import java.util.Arrays;
 public class ReverseAString {
 
     public static void main(String args[]) {
-
         String giveString = "INDIA";
+        System.out.println(usingSwapingReverse(giveString));
+
+    }
+
+    public static String reverseWithCharArray(String giveString) {
         char[] stringtoArray = giveString.toCharArray();
-
-        for (int i = stringtoArray.length-1; i >= 0; i--) {
-            System.out.print(stringtoArray[i]);
+        char[] result = new char[stringtoArray.length];
+        for (int i = stringtoArray.length - 1; i >= 0; i--) {
+            result[stringtoArray.length - i - 1] = stringtoArray[i];
         }
+        return Arrays.toString(result);
+    }
 
+    public static String reverseWithBytesArray(String giveString) {
+        byte[] stringtoArray = giveString.getBytes();
+        byte[] result = new byte[stringtoArray.length];
+        for (int i = stringtoArray.length - 1; i >= 0; i--) {
+            result[stringtoArray.length - i - 1] = stringtoArray[i];
+        }
+        return new String(result);
+    }
+
+    public static String stringBuilderReverse(String giveString) {
+        StringBuilder sf = new StringBuilder(giveString);
+        sf.reverse();
+        return sf.toString();
+    }
+
+    public static String usingSwapingReverse(String giveString) {
+        char[] stringArray = giveString.toCharArray();
+        int left, right = 0;
+        right = stringArray.length - 1;
+
+        for (left = 0; left < right; left++, right--) {
+            char temp = stringArray[left];
+            stringArray[left] = stringArray[right];
+            stringArray[right] = temp;
+        }
+        return Arrays.toString(stringArray);
     }
 
 }
